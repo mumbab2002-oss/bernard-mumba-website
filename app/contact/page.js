f89c7625431181241cb0d8ae09d29cdc62948774
mumbab2002-oss <mumbab2002@gmail.com>
2026-01-04 21:22:30 +0200

    Update page.js

--- app/contact/page.js
+++ app/contact/page.js
@@ -24,3 +24,20 @@ export default function Contact() {
     </div>
   );
 }
+<h2>Send Us a Message</h2>
+
+<form
+  action="https://formsubmit.co/mumbab2002@gmail.com"
+  method="POST"
+>
+  <input type="hidden" name="_subject" value="New Inquiry - Mumba Consulntants" />
+  <input type="hidden" name="_captcha" value="false" />
+
+  <input required name="name" placeholder="Your Name" />
+  <br /><br />
+  <input required name="email" placeholder="Your Email" />
+  <br /><br />
+  <textarea required name="message" placeholder="Your Message"></textarea>
+  <br /><br />
+  <button type="submit">Send Message</button>
+</form>
