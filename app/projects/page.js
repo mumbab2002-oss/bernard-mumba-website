15f53b024b476e6aed3e9da50138b01853c0a4e7
mumbab2002-oss <mumbab2002@gmail.com>
2026-01-05 14:41:10 +0200

    Update page.js

--- app/projects/page.js
+++ app/projects/page.js
@@ -1,13 +1,4 @@
 export default function Projects() {
-  const imgStyle = {
-    width: "300px",
-    height: "200px",
-    objectFit: "cover",
-    margin: "10px",
-    borderRadius: "8px",
-  };
-
-export default function Projects() {
   return (
     <div>
       <h1>Our Projects</h1>
@@ -26,26 +17,3 @@ const grid = {
   gridTemplateColumns: "repeat(auto-fit, minmax(250px, 1fr))",
   gap: "15px",
 };
-
-  return (
-    <div>
-      <h1>Our Projects</h1>
-      <p>
-        Below are examples of residential buildings, site supervision works, and
-        infrastructure-related projects handled or supervised by Mumba
-        Consulntants.
-      </p>
-
-      <div style={{ display: "flex", flexWrap: "wrap" }}>
-        <img src="/images/project1.jpg" style={imgStyle} />
-        <img src="/images/project2.jpg" style={imgStyle} />
-        <img src="/images/project3.jpg" style={imgStyle} />
-      </div>
-
-      <p>
-        upload images into the <strong>public/images</strong>
-  
-      </p>
-    </div>
-  );
-}
