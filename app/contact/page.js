66f5c89b9a25c881a5de13a41e6376af2e2eef1b
mumbab2002-oss <mumbab2002@gmail.com>
2026-08-21 21:49:41 +0200

    commit

--- app/contact/page.js
+++ app/contact/page.js
@@ -1,24 +1,4 @@
-import { useEffect } from "react";
-
-function loadScript(callback) {
-  var script = document.createElement("script");
-  script.type = "text/javascript";
-  script.async = true;
-  script.src = "https://tracker.metricool.com/resources/be.js";
-  script.onload = callback;
-  script.onerror = function () {
-    console.error("Failed to load Metricool tracker script");
-  };
-  document.getElementsByTagName("head")[0].appendChild(script);
-}
-
 export default function Contact() {
-  useEffect(() => {
-    loadScript(function () {
-      window.beTracker.t({ hash: "6e72d75ae0c630f71423e187233e2cc7" });
-    });
-  }, []);
-
   return (
     <div>
       <h2>Contact Us</h2>
